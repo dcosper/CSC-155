3efbb21e13105e3187a503f50b7e9fe7c2971477
Dylan Cosper <dcosper1@stu.parkland.edu>
2023-04-20 09:09:06 -0500

    Add navbar to header

--- library/header.php
+++ library/header.php
@@ -1,10 +1,30 @@
+<?php
+function echo_navbar_page(string $url, string $title, float $role = ROLE_GUEST) {
+	if ($_SESSION["role"] >= $role) {
+		echo "<li><a href='$url'>$title</a></li>";
+	}
+}
+?>
+
 <header>
 	<a href="index.php"><h1>The Coolest Site</h1></a>
 	<?php
 	if (isset($_SESSION["username"])) {
-		echo "<p>Logged in as: {$_SESSION["username"]}</p>";
+		echo "<p>";
+		echo "Logged in as: {$_SESSION["username"]}";
+		echo "<br>";
 		echo "<a href='?logout=1'>Log out</a>";
+		echo "</p>";
 	}
 	?>
+	<ul class="navbar">
+		<?php echo_navbar_page("index.php", "Home") ?>
+		<?php echo_navbar_page("users.php", "Users", ROLE_USER) ?>
+		<?php echo_navbar_page("shop1.php", "Item 1", ROLE_USER) ?>
+		<?php echo_navbar_page("shop2.php", "Item 2", ROLE_USER) ?>
+		<?php echo_navbar_page("shop3.php", "Item 3", ROLE_USER) ?>
+		<?php echo_navbar_page("shop4.php", "Item 4", ROLE_USER) ?>
+		<?php echo_navbar_page("cart.php", "Cart", ROLE_USER) ?>
+	</ul>
 	<hr>
 </header>

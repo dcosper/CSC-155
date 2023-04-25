b170dacc8a376ab04ec49f783f8fd62878fda235
Dylan Cosper <dcosper1@stu.parkland.edu>
2023-04-25 09:19:00 -0500

    Add other shops

---
+++ shop2.php
@@ -0,0 +1,10 @@
+<!--  I honor Parkland's core values by affirming that I have 
+followed all academic integrity guidelines for this work.
+Dylan Cosper
+CSC-155-001DR_2023SP -->
+<?php
+$ITEM_NAME = "Logistic Science Pack";
+$ITEM_IMAGE = "logistic_pack.png";
+$ITEM_INDEX = "1";
+require("lib/shop.php");
+?>
---
+++ shop4.php
@@ -0,0 +1,10 @@
+<!--  I honor Parkland's core values by affirming that I have 
+followed all academic integrity guidelines for this work.
+Dylan Cosper
+CSC-155-001DR_2023SP -->
+<?php
+$ITEM_NAME = "Production Science Pack";
+$ITEM_IMAGE = "production_pack.png";
+$ITEM_INDEX = "3";
+require("lib/shop.php");
+?>
